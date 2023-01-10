4c8f999b5e02272bd7fa72b2e06a5df392ebbf4c
jaeseo <jaeserrr@ewhain.net>
2023-01-10 15:26:33 +0900

    [CHORE] 포맷팅

--- Assets/Scripts/DamageManager.cs
+++ Assets/Scripts/DamageManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DamageManager : MonoBehaviour
-{
+public class DamageManager : MonoBehaviour {
     Slider damageBarValue;  // 데미지바 value
     GameObject player;      // player의 데미지 값에 접근하기 위해 태그로 찾음
     float playerDamageValue;// player의 데미지 value
@@ -16,7 +15,7 @@ public class DamageManager : MonoBehaviour
     void Update() {
         player = GameObject.FindWithTag("Player");
 
-        playerDamageValue= player.GetComponent<Player001>().damage; // Player001의 데미지
+        playerDamageValue = player.GetComponent<Player001>().damage; // Player001의 데미지
 
         damageBarValue.value = playerDamageValue;
     }

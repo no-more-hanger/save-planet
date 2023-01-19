0b43b1e5cc1226e2d8377e233c0d3a30b7559fa3
jaeseo <jaeserrr@ewhain.net>
2023-01-20 03:01:53 +0900

    [FIX] Fix Moving

--- Assets/Scripts/Character/BaseCharacter.cs
+++ Assets/Scripts/Character/BaseCharacter.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BaseCharacter : MonoBehaviour {
     private string LAYER_ENEMY = "Enemy";
+    private string LAYER_DISASTER = "Disaster";
+    private string LAYER_ITEM = "Item";
+
+    private int duration = 100;// 효과 지속 상태
+
     private float gravityScale = 0.5f;      // 중력 크기
 
     protected Transform position; // 현재 위치
@@ -18,10 +23,12 @@ public class BaseCharacter : MonoBehaviour {
 
     protected int balloonCnt;               // 소지한 풍선 개수
 
-    protected float noDamageTimer = 1f;   // 무적 시간
+    protected float noDamageTimer = 1f;     // 무적 시간
     protected Rigidbody2D rigidbody;
 
     public ParticleSystem effect;
+
+    private Vector2 damagedVelocity;        // 데미지 받았을 시, 튕겨나가는 방향
 
     // 항상 업데이트 될 부분들
     protected void StateUpdate() {
@@ -39,7 +46,7 @@ public class BaseCharacter : MonoBehaviour {
 
         // 한 프레임 당 이동거리 계산
         float moveX = x * speed * Time.deltaTime;
-        float moveY = y * speed * Time.deltaTime;
+        float moveY = (y * speed - gravityScale) * Time.deltaTime;
 
         // Idle
         if (x == 0.0f) {
@@ -62,41 +69,37 @@ public class BaseCharacter : MonoBehaviour {
         if (position.position.x > width) {
             position.position = new Vector3(width, position.position.y, 0);
         }
-
-        // 중력 제어
-        if (noDamageTimer > 0) return;  // 공격 받았을 땐 튕겨나가는 velocity 적용을 위해 중력 제어 안 받고 return
-        rigidbody.velocity = new Vector3(0, -gravityScale, 0); // 오브젝트 y방향으로 끌어내림
-    }
-
-    // 속도 조절
-    public void ControlSpeed(float variation) {
-        speed += variation;
     }
 
     private void OnCollisionStay2D(Collision2D collision) {
-        if (collision.gameObject.layer == LayerMask.NameToLayer(LAYER_ENEMY)) {
-            Vector2 velocity = Vector2.zero;    // Vector2(0, 0) 초기화
+        int collsionLayer = collision.gameObject.layer;
 
-            // 적보다 오른쪽에 있으면 오른쪽(1)으로, 왼쪽에 있으면 왼쪽(-1)으로 튀도록
-            velocity.x = (transform.position.x - collision.transform.position.x) > 0 ? 1 : -1;
-            velocity.y = -1; // 무조건 아래로 튀도록
+        // 장애물 || 재해
+        if (collsionLayer == LayerMask.NameToLayer(LAYER_ENEMY) || collsionLayer == LayerMask.NameToLayer(LAYER_DISASTER)) {
+            damagedVelocity = new Vector2(0, -1);    // 무조건 아래로 튀도록 초기화
 
-            rigidbody.velocity = velocity * 1.5f;  // 튀어나가는 정도
+            // 장애물 || 재해보다 오른쪽에 있으면 오른쪽(1)으로, 왼쪽에 있으면 왼쪽(-1)으로 튀도록
+            damagedVelocity.x = (transform.position.x - collision.transform.position.x) > 0 ? 1 : -1;
 
-            Hurt(50);                           // (임시) item에서 제어
+            Hurt(10);                           // (임시) item에서 제어
+        }
+        // 아이템
+        if (collsionLayer == LayerMask.NameToLayer(LAYER_ITEM)) {
+            Heal(50);                           // (임시) item에서 제어
         }
     }
 
+
     // 데미지 입음
     public void Hurt(float hurt) {
-        if (noDamageTimer > 0) return;  // 무적일 땐 return
+        if (noDamageTimer > 0) return;      // 무적일 땐 return
 
-        noDamageTimer = 1.0f;           // 무적 시간 시작
-        damage += hurt;                 // 데미지 입음
+        noDamageTimer = 1.0f;               // 무적 시작
+        damage += hurt;                     // 데미지 입음
 
-        StartCoroutine(GetDamagedRoutine());
+        StartCoroutine(GetDamagedRoutine());// 데미지 입은 효과
 
-        if (damage >= 100) {            // 죽음
+        if (damage >= 100) {                // 죽음
             rigidbody.simulated = false;
             spriteRenderer.enabled = false;
             GetComponent<Collider2D>().enabled = false;
@@ -108,22 +111,41 @@ public class BaseCharacter : MonoBehaviour {
         }
     }
 
+    // 데미지 치료
+    public void Heal(float heal) {
+        damage -= heal;
+        StartCoroutine(GetHealedRoutine());
+    }
+
+
     public IEnumerator GetDamagedRoutine() {
-        for (int i = 0; i <= 100; i++) {
-            spriteRenderer.color = new Color(1, 0.01f * i, 0.01f * i);  // 빨간색
+        for (int i = 0; i <= duration; i++) {
+            spriteRenderer.color = new Color(1, 0.01f * i, 0.01f * i);  // 빨간색으로 변함
+
+            // 튕겨 나감
+            Vector2 dv = damagedVelocity * (duration - i) * 0.1f * Time.deltaTime;
+            position.Translate(new Vector3(dv.x, dv.y, 0));
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    public IEnumerator GetHealedRoutine() {
+        for (int i = 0; i <= duration; i++) {
+            spriteRenderer.color = new Color(0.01f * i, 1, 0.01f * i);  // 초록색
             yield return new WaitForSeconds(0.01f);
         }
     }
 
-    // 데미지 치료
-    public void Heal(float heal) {
-        damage -= heal;
+    // 속도 조절
+    public void ControlSpeed(float variation) {
+        speed += variation;
     }
 
     // 총 메기
     public void PutOnGun() {
         isGun = true;
     }
+
     // 총 내려놓기
     public void PutDownGun() {
         isGun = false;

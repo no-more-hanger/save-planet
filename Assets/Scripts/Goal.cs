d236fc6cf5e4a5f9e9a54cab7c1773dbf6256b16
jiyoung <jiyoung_06@ewhain.net>
2023-02-03 01:40:52 +0900

    [ADD] ending score

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Goal : MonoBehaviour {
+    public TimerController timer;
+    private void Start() {
+        timer = GameObject.FindWithTag("Timer").GetComponent<TimerController>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        timer.StopTimer();
+        GameObject.Find("Canvas").transform.Find("EndingPopup").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.Find("SettingPopup").GetComponent<SettingManager>().OnPauseGame();
+        GameObject.Find("PlayTimeText").GetComponent<TextMeshProUGUI>().text = timer.GetTimeString();
+    }
+}

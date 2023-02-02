a813d9f282f3925fe0f82fc182d17b07b5783537
jiyoung <jiyoung_06@ewhain.net>
2023-02-02 23:02:40 +0900

    [ADD] trash falling down

--- Assets/Scripts/Elements/Trash.cs
+++ Assets/Scripts/Elements/Trash.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Trash : BaseElement {
     private float damage = 10;
+    private float speed = 1;
     protected override void AdjustEffect() {
         playerScript.Hurt(damage, transform.position);
+    }
+
+    private void Update() {
+        transform.Translate(Vector3.down * Time.deltaTime * speed);
     }
 }

df84d0a14703e44fc8a40ccb409084339732b0d8
suabae <bsa0322@ewhain.net>
2023-02-04 03:25:21 +0900

    [CHORE] destroy bullet

--- Assets/Scripts/Character/BulletController.cs
+++ Assets/Scripts/Character/BulletController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
+    private float destroyDistance = 5.0f;
 
     // Update is called once per frame
     void Update()
     {
+        if (Camera.main.transform.position.y + destroyDistance + GetComponent<SpriteRenderer>().sprite.bounds.size.y < transform.position.y) {
+            Destroy(gameObject);
+        }
+
         transform.Translate(Vector3.up * Time.deltaTime * speed, Space.World);
     }
 }
